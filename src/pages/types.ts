4735485f498a76f050b8cc1448f09a333f4fea57
parkjeehoon <gkstj8300@naver.com>
2024-12-23 10:36:30 +0900

    fix: layout type 관련 필요없는 코드 제거

--- src/pages/types.ts
+++ src/pages/types.ts
@@ -12,19 +12,3 @@ export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
 export type AppPropsWithLayout = AppProps & {
 	Component: NextPageWithLayout;
 };
-
-export const LayoutType = {
-	DEFAULT: 1,
-	PURCHASE_LINK: 2,
-	INDUSTRY: 3,
-} as const;
-
-export type LayoutType = (typeof LayoutType)[keyof typeof LayoutType];
-
-export const LoginSession = {
-	NOT_LOGIN_OR_INVALID_SESSION: '1',
-	LOGIN_WITHIN_30_MIN: '2',
-	LOGIN_OVER_30_MIN: '3',
-} as const;
-
-export type LoginSession = (typeof LoginSession)[keyof typeof LoginSession];
