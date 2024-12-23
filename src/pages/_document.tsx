217b454f3fd62b523e29429cbee72f90c2fecbfa
parkjeehoon <gkstj8300@naver.com>
2024-12-23 15:04:45 +0900

    style: font 적용 테스트

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,11 @@ class Document extends RawDocument {
     render() {
         return (
             <Html lang="ko">
-                <Head />
+				<Head>
+                    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                    <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Oswald:wght@200..700&display=swap" rel="stylesheet" />
+				</Head>
                 <body>
                     <Main />
                     <NextScript />
