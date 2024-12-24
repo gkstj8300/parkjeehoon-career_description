c9d806ec010056332e772bd696906f94d2802615
parkjeehoon <gkstj8300@naver.com>
2024-12-24 17:56:13 +0900

    feat: favicon 삽입

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ class Document extends RawDocument {
         return (
             <Html lang="ko">
 				<Head>
+                    <link rel="icon" href="/favicon.ico" />
                     <link rel="preconnect" href="https://fonts.googleapis.com"/>
                     <link rel="preconnect" href="https://fonts.gstatic.com"/>
                     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Oswald:wght@200..700&display=swap" rel="stylesheet" />
