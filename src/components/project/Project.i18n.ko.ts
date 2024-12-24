bd9f186cabcc85ad5de2fdc92b03b9898be3bdef
parkjeehoon <gkstj8300@naver.com>
2024-12-24 16:46:27 +0900

    fix: Project text 수정

--- src/components/project/Project.i18n.ko.ts
+++ src/components/project/Project.i18n.ko.ts
@@ -3,22 +3,22 @@ import { Translation } from '@/i18n/types';
 export const project: Translation = {
     title: 'Project',
     hanwha: {
-        name: '한화생명',
+        name: '한화생명 코어 프로젝트',
         duration: '2022.03 - 2022.12',
         skillKeywords: 'Java,Javascript,Servlet,Apache,Oracle,KendoUI,GitLab,JobPass,TeraStream,ETL',
     },
     naedam: {
-        name: '내담C&C',
+        name: '내담C&C 사내 홈페이지 신규개발',
         duration: '2022.12 - 2023.03',
         skillKeywords: 'Java,Spring Boot,Javascript,Jquery,MariaDB,Servlet,Cafe24',
     },
     misumiMaintenance: {
-        name: '한국미스미(유지보수)',
+        name: '한국미스미 프론트엔드 개발/유지보수/운영',
         duration: '2023.03 - 2024-03 ',
         skillKeywords: 'Php,Rxjs,Twig,Javascript,Jquery,Ubuntu Linux,Docker,Apache,Nodejs,Xampp,Bitbucket,Jenkins',
     },
     misumiRenewal: {
-        name: '한국미스미(리뉴얼)',
+        name: '한국미스미 리뉴얼 개발',
         duration: '2024.03 - 진행중 ',
         skillKeywords: 'React,TypeScript,Next.js,Redux,Nodejs,Sass,Vercel,Webpack,SWC,Babel,DataDog,GitHub Actions,Circleci',
     }
