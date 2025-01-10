89579979d3ccd46549951d328e354df787a52f36
parkjeehoon <gkstj8300@naver.com>
2025-01-10 18:00:38 +0900

    fix: 다국어 아이콘 위치 조정 및 푸쉬 1차

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,6 +1,44 @@
+import i18n from 'i18next';
+import React, { useState, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 import styles from './Header.module.scss';
 
 export const Header: React.FC = () => {
     const { t } = useTranslation();
+    const [isHover, setIsHover] = useState(false);
+
+    const handleLocaleChageClick = useCallback((locale: string) => {
+        i18n.changeLanguage(locale);
+    }, []);
+
+    return (
+        <div className={styles.local}>
+            <div 
+                className={styles.localeWrap}
+                data-hover={isHover}
+                onMouseLeave={() => setIsHover(false)}
+            >
+                <ul>
+                    <li>
+                        <a 
+                            className={styles.localeLink} 
+                            href="#"
+                            onClick={() => handleLocaleChageClick('ko')}
+                        >KOR</a>
+                    </li>
+                    <li>
+                        <a 
+                            className={styles.localeLink} 
+                            href="#"
+                            onClick={() => handleLocaleChageClick('en')}
+                        >ENG</a>
+                    </li>
+                </ul>
+            </div>
+            <i 
+                className={styles.localeIcon}
+                onMouseOver={() => setIsHover(true)}
+            ></i>
+        </div>
+    )
 }
