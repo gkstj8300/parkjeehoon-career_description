3ec21561744e6bd098628e1549fc63f04ba5a8a6
parkjeehoon <gkstj8300@naver.com>
2025-01-10 17:10:45 +0900

    fix: header 주석

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import styles from './Layout.module.scss';
 import { Footer } from "./footer"
-import { Header } from './header';
+// import { Header } from './header';
 
 type LayoutProps = {
     children: React.ReactNode;
@@ -10,7 +10,7 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
 
     return (
         <div className={styles.layout}>
-            <Header />
+            {/* <Header /> */}
             <div className={styles.main}>
                 <div className={styles.childrenWrapper}>{children}</div>
             </div>
