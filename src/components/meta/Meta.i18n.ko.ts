abef3d2f1f56a70bf6e47e83297ee4900097b3ac
parkjeehoon <gkstj8300@naver.com>
2025-01-22 13:52:34 +0900

    fix: og 태그 삽입 및 robots.txt 설정 변경

--- src/components/meta/Meta.i18n.ko.ts
+++ src/components/meta/Meta.i18n.ko.ts
@@ -2,7 +2,7 @@ import { Translation } from '@/i18n/types';
 
 export const meta: Translation = {
 	title: 'Jeehoon | FrontEnd Portfolio',
-	description: '반갑습니다, 프론트엔드 개발자 박지훈입니다. 백엔드와 프론트엔드 개발, 코어 개발, 신규/리뉴얼 개발, 유지보수 및 운영 경험을 지니고 있습니다. 비즈니스와 사용자와의 적극적인 커뮤니케이션과 책임감을 바탕으로 보다 더 좋은 프론트엔드 개발자로서 성장하기 위해 학습하고 경험하고 노력하겠습니다.',
+	description: '반갑습니다, 프론트엔드 개발자 박지훈입니다. 프론트엔드 개발, 코어 개발, 신규/리뉴얼 개발, 유지보수 및 운영 경험을 지니고 있습니다.',
 	keyword: '프론트엔드, 개발자, 프론트엔드 개발자, 리액트, 리액트 개발자, SPA, SPA 개발자, 리뉴얼 개발, 유지보수, 운영',
 	viewport: 'width=device-width, initial-scale=1',
 };
--- src/components/meta/Meta.tsx
+++ src/components/meta/Meta.tsx
@@ -10,6 +10,9 @@ export const Meta = () => {
             <meta name="description" key="descriptionKey" content={t('common.meta.description')} />
 			<meta name="keywords" key="keywordsKey" content={t('common.meta.keyword')} />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
+			<meta property="og:title" key="ogTitleKey" content={t('common.meta.title')} />
+			<meta property="og:description" key="ogDescriptionKey" content={t('common.meta.description')} />
+			<meta property="og:url" key="ogUrlKey" content='https://parkjeehoon-portfolio.vercel.app/' />
 			<meta name="naver-site-verification" content="dce7684b42ec210aee7522c5eaee271cd670182d" />
 		</Head>
 	);
